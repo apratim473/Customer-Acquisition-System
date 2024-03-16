447288d21c3f9f83935c9541b146c3c3836fb534
apratim473 <apratim473@gmail.com>
2024-03-16 14:00:05 +0530

    Add spring initializer files
    Java files were missing in previous commit, hence added the same in this commit.

---
+++ src/main/java/com/cas/CustomerAcquisitionSystem/CustomerAcquisitionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cas.CustomerAcquisitionSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAcquisitionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAcquisitionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cas/CustomerAcquisitionSystem/CustomerAcquisitionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cas.CustomerAcquisitionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAcquisitionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
